14839820e7e1314806abd355e77ba79af51d30bd
wolfingten <jagu00005@stud.uni-saarland.de>
2025-12-31 12:42:24 +0100

    venv setup

--- flake.nix
+++ flake.nix
@@ -1,11 +1,13 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = {
     self,
     nixpkgs,
+    nixpkgs-unstable,
     ...
   }: let
     systems = [
@@ -14,16 +16,22 @@
       "x86_64-darwin"
       "aarch64-darwin"
     ];
+    overlay-unstable = final: prev: {
+      unstable = nixpkgs-unstable.legacyPackages.${prev.system};
+    };
 
     forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
     devShells = forAllSystems (
       system: let
-        pkgs = import nixpkgs {inherit system;};
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [overlay-unstable];
+        };
       in {
         default = pkgs.mkShell {
           packages = with pkgs; [
-            bootdev-cli
+            unstable.bootdev-cli
             uv
             python312
           ];
